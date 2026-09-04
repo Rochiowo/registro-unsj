9fb25100b212d24f3bf1363173b19cd04c804d1a
Rocio <rociorecio05@gmail.com>
2026-09-04 14:37:32 -0300

    feat: implementación de rutas, vistas de listado y edición

---
+++ resources/views/home.blade.php
@@ -0,0 +1,2 @@
+<?php
+#comentado por mi
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('css/movies.css') }}">
+
 <main class="movies-page">
     <div class="movies-shell">
 
@@ -237,4 +239,3 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 </script>
 @endsection
-
